25a786fadfd3a0345c5c64c629bf1c3ee6bfd493
jpyatachkov <cvkucherov@yandex.ru>
2018-06-17 13:09:47 +0300

    Reformat and index for parent_tree

--- src/main/resources/db/migration/V2__forum_users.sql
+++ src/main/resources/db/migration/V2__forum_users.sql
@@ -1,8 +1,8 @@
 CREATE TABLE forum_users (
-  forum_slug    CITEXT COLLATE "C" NOT NULL,
-  nickname  CITEXT COLLATE "C",
-  email     CITEXT UNIQUE,
-  full_name TEXT NOT NULL,
-  about     TEXT NOT NULL,
+  forum_slug CITEXT COLLATE "C" NOT NULL,
+  nickname   CITEXT COLLATE "C",
+  email      CITEXT UNIQUE,
+  full_name  TEXT               NOT NULL,
+  about      TEXT               NOT NULL,
   UNIQUE (forum_slug, nickname)
 );
--- src/main/resources/db/migration/V3__indexes.sql
+++ src/main/resources/db/migration/V3__indexes.sql
@@ -1,18 +1,17 @@
 CREATE INDEX forum_id_created_at_idx
   ON threads (forum_id, created_at);
 
-CREATE INDEX thread_id_id_idx
-  ON posts (thread_id, id);
-CREATE INDEX thread_id_materialized_path_idx
-  ON posts (thread_id, materialized_path);
-
 CREATE INDEX post_thread_id_id_idx
   ON posts (thread_id, id);
-CREATE INDEX post_thread_id_materialized_path_idx
-  ON posts (thread_id, materialized_path);
-CREATE INDEX post_thread_id_parent_id_root_id_id_idx
-  ON posts (thread_id, parent_id, root_id, id);
-CREATE INDEX post_root_id_id_materialized_path_idx
-  ON posts (root_id, id, materialized_path);
-CREATE INDEX post_root_id_materialized_path_desc_id_idx
-  ON posts (root_id, materialized_path DESC, id);
+CREATE INDEX post_root_id_id_idx
+  ON posts (root_id, id);
+CREATE INDEX post_id_root_id_materialized_path
+  ON posts (id, root_id, materialized_path);
+-- CREATE INDEX post_thread_id_materialized_path_idx
+--   ON posts (thread_id, materialized_path);
+-- CREATE INDEX post_thread_id_parent_id_root_id_id_idx
+--   ON posts (thread_id, parent_id, root_id, id);
+-- CREATE INDEX post_root_id_id_materialized_path_idx
+--   ON posts (root_id, id, materialized_path);
+-- CREATE INDEX post_root_id_materialized_path_desc_id_idx
+--   ON posts (root_id, materialized_path DESC, id);
